728654a9a46dcc8c3705bd8c45d000bcb201163a
Tenutti00 <30781@raphaeldisanto.com.br>
2026-04-15 08:08:19 -0300

    Add files via upload

---
+++ IA2-Tri/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+     <div className="imc-container">
+      <div className="imc-card">
+        <div className="imc-header">
+          <h1>Calculadora IMC</h1>
+          <p>Descubra seu índice de massa corporal</p>
+        </div>
+
+        <div className="imc-form">
+          <input 
+            type="number" 
+            placeholder='Ex: 70' 
+          />
+          <input type="number" 
+            placeholder='Ex: 1.75' 
+          />
+          <button>Calcular</button>
+          <button>Limpar</button>
+        </div>
+
+        <div className="result-card">
+         <h1>22.9</h1>
+         <p>Peso Normal</p>
+         <small>Índice de Massa Corporal</small>
+        </div>
+
+      </div> {/* Fecha Card */}
+     </div> {/* Fecha Container */}
+    </>
+  )
+}
